6ffd878f7ba4d704e9dcde846fdf350ed7c050ce
Austin Douse <douse.austin@gmail.com>
2020-08-07 15:06:19 -0400

    add basic pagination

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,16 @@ import fetch from "isomorphic-fetch";
 import Error from "next/error";
 import StoryList from "../components/StoryList";
 import Layout from "../components/Layout";
+import Link from "next/link";
 
 class Index extends React.Component {
-  static async getInitialProps() {
+  static async getInitialProps({req, res, query}) {
     let stories;
+    let page;
     try {
+      page = Number(query.page) || 1;
       const response = await fetch(
-        "http://node-hnapi.herokuapp.com/news?page=1"
+        `http://node-hnapi.herokuapp.com/news?page=${page}`
       );
       stories = await response.json();
     } catch (err) {
@@ -18,16 +21,35 @@ class Index extends React.Component {
 
     return {
       stories,
+      page,
     };
   }
   render() {
-    const {stories} = this.props;
+    const {stories, page} = this.props;
     if (stories.length === 0) {
       return <Error statusCode={503} />;
     }
     return (
-      <Layout>
+      <Layout
+        title="Hacker News Next"
+        description="A Hacker News clone made with next.js"
+      >
         <StoryList stories={stories} />
+        <footer>
+          <Link href={`/?page=${page + 1}`}>
+            <a>Next Page ({page + 1})</a>
+          </Link>
+        </footer>
+        <style jsx>{`
+          footer {
+            padding: 1em;
+          }
+          footer a {
+            font-weight: bold;
+            color: black;
+            text-decoration: none;
+          }
+        `}</style>
       </Layout>
     );
   }
