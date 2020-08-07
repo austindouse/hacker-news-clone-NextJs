c03d68288bf36851845c6e5ef0fb870c80be5bb9
Austin Douse <douse.austin@gmail.com>
2020-08-07 14:52:20 -0400

    add layout component

---
+++ components/Layout.js
@@ -0,0 +1,45 @@
+import Link from "next/link";
+
+const Layout = ({children}) => (
+  <div>
+    <div className="container">
+      <nav>
+        <Link href="/">
+          <a>
+            <span className="main-title">Hacker News Next</span>
+          </a>
+        </Link>
+      </nav>
+      {children}
+    </div>
+    <style jsx>{`
+      .container {
+        max-width: 800px;
+        margin: 0 auto;
+        background: #f6f6ef;
+      }
+      nav {
+        background: #f60;
+        padding: 1em;
+      }
+      nav > * {
+        display: inline-block;
+        color: black;
+      }
+      nav a {
+        text-decoration: none;
+      }
+      nav .main-title {
+        font-weight: bold;
+      }
+    `}</style>
+    <style global>{`
+     body {
+         background: white; 
+         font-family: Verdena, Geneva, sans-serif; 
+     }
+    `}</style>
+  </div>
+);
+
+export default Layout;
